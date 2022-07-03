432bfc5f1d62a53e2b1035446c19b498c993ba50
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-03 11:14:31 -0300

    button component initial code

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ text, onClick }) {
+function Button(props) {
+  const {
+    type, text, onClick, disabled,
+  } = props;
   return (
     <button
-      type="button"
+    /* eslint-disable react/button-has-type */
+      type={type || 'button'}
+    /* eslint-enable react/button-has-type */
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
@@ -14,11 +20,14 @@ function Button({ text, onClick }) {
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']).isRequired,
   onClick: PropTypes.func,
+  disabled: PropTypes.bool,
 };
 
 Button.defaultProps = {
   onClick: () => {},
+  disabled: false,
 };
 
 export default Button;
