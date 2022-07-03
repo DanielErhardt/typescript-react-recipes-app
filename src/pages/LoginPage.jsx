71c7b98e2b1563eaa87d4035c0afdd355c63d4d6
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-03 11:15:55 -0300

    login page structure created

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import InputField from '../components/InputField';
+import Button from '../components/Button';
+
+const submitLogin = () => {
+
+};
 
 function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div className="page-container login-page">
+      <section className="page-content">
+        <h1>Login</h1>
+        <InputField type="email" placeholder="Email" />
+        <InputField type="password" placeholder="Password" />
+        <Button type="button" text="Enter" onClick={submitLogin} />
+      </section>
+    </div>
+
   );
 }
 
