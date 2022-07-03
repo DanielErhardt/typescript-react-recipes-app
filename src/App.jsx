8ef8ddba66286fef3f0dc2f6f8e7e2f9f24bd9db
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-03 11:06:17 -0300

    changed Switch to Routes for the new React version

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecipesProvider from './context/RecipesProvider';
 import LoginPage from './pages/LoginPage';
 import UserProfile from './pages/UserProfile';
@@ -10,43 +10,42 @@ import ExploreByIngredients from './pages/ExploreByIngredients';
 import ExploreByNationalities from './pages/ExploreByNationalities';
 import UserRecipes from './pages/UserRecipes';
 import NotFound from './pages/NotFound';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <RecipesProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={LoginPage} />
-            <Route exact path="/foods" component={MainPage} />
-            <Route exact path="/drinks" component={MainPage} />
-            <Route path="/foods/:foodId" component={RecipePage} />
-            <Route path="/drinks/:drinkId" component={RecipePage} />
-            <Route path="/foods/:foodId/in-progress" component={RecipePage} />
-            <Route path="/drinks/:drinkId/in-progress" component={RecipePage} />
-            <Route exact path="/explore" component={ExplorePage} />
-            <Route path="/explore/foods" component={ExplorePage} />
-            <Route path="/explore/drinks" component={ExplorePage} />
-            <Route
-              path="/explore/foods/ingredients"
-              component={ExploreByIngredients}
-            />
-            <Route
-              path="/explore/drinks/ingredients"
-              component={ExploreByIngredients}
-            />
-            <Route
-              path="/explore/foods/nationalities"
-              component={ExploreByNationalities}
-            />
-            <Route path="/profile" component={UserProfile} />
-            <Route path="/done-recipes" component={UserRecipes} />
-            <Route path="/favorite-recipes" component={UserRecipes} />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </BrowserRouter>
-      </RecipesProvider>
-    </div>
+    <RecipesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route exact path="/foods" element={<MainPage />} />
+          <Route exact path="/drinks" element={<MainPage />} />
+          <Route path="/foods/:foodId" element={<RecipePage />} />
+          <Route path="/drinks/:drinkId" element={<RecipePage />} />
+          <Route path="/foods/:foodId/in-progress" element={<RecipePage />} />
+          <Route path="/drinks/:drinkId/in-progress" element={<RecipePage />} />
+          <Route exact path="/explore" element={<ExplorePage />} />
+          <Route path="/explore/foods" element={<ExplorePage />} />
+          <Route path="/explore/drinks" element={<ExplorePage />} />
+          <Route
+            path="/explore/foods/ingredients"
+            element={<ExploreByIngredients />}
+          />
+          <Route
+            path="/explore/drinks/ingredients"
+            element={<ExploreByIngredients />}
+          />
+          <Route
+            path="/explore/foods/nationalities"
+            component={<ExploreByNationalities />}
+          />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/done-recipes" element={<UserRecipes />} />
+          <Route path="/favorite-recipes" element={<UserRecipes />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </RecipesProvider>
   );
 }
 
