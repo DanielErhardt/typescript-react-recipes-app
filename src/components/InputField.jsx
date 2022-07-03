7f26decec9680161ba311daf3adcad87fa06eea1
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-03 11:11:02 -0300

    input field component initial code

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import createId from '../Helpers/HelperFunctions';
 
-function InputField(props) {
+function InputField({ type, label, placeholder }) {
+  const id = createId();
+
   return (
-    <div>InputField</div>
+    <div className="input-field-component">
+      <label htmlFor={id}>
+        {`${label} `}
+        <input id={id} type={type} placeholder={placeholder} />
+      </label>
+    </div>
   );
 }
 
-InputField.propTypes = {};
+InputField.defaultProps = {
+  label: '',
+  placeholder: '',
+};
+
+InputField.propTypes = {
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  type: PropTypes.string.isRequired,
+};
 
 export default InputField;
