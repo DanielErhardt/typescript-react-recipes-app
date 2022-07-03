e417d388b8346198003c2804d801efc4d9cc0aca
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-03 11:12:22 -0300

    added conditional icons for email and password

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
+import { At, Password } from 'phosphor-react';
 import PropTypes from 'prop-types';
 import createId from '../Helpers/HelperFunctions';
 
 function InputField({ type, label, placeholder }) {
   const id = createId();
 
+  const renderIcon = () => {
+    const config = {
+      weight: 'bold',
+      size: '24px',
+      color: 'black',
+    };
+
+    switch (type) {
+      case 'email':
+        return (
+          <At
+            weight={config.weight}
+            size={config.size}
+            color={config.color}
+          />
+        );
+
+      case 'password':
+        return (
+          <Password
+            weight={config.weight}
+            size={config.size}
+            color={config.color}
+          />
+        );
+
+      default:
+        return ('');
+    }
+  };
+
   return (
     <div className="input-field-component">
+      {label === '' && renderIcon()}
       <label htmlFor={id}>
         {`${label} `}
         <input id={id} type={type} placeholder={placeholder} />
