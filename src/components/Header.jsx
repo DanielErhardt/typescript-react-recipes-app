eb6333e2544d7c9eec402538ae609a490044b762
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-04 19:52:00 -0300

    imported and used a few elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { MagnifyingGlass } from 'phosphor-react';
 import PropTypes from 'prop-types';
+import InputElement from './inputs/InputElement';
+import { defaultIconConfig as ic } from '../helpers';
 
 function Header({ title }) {
   return (
     <section className="header">
       <h1 className="header-title">{title}</h1>
+      <div className="header-search-bar">
+        <InputElement
+          label={<MagnifyingGlass weight={ic.weight} color={ic.color} size={ic.size} />}
+          type="search"
+        />
+      </div>
     </section>
   );
 }
