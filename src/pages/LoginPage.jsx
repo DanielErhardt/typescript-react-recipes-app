6b0dcb4d11fec45646e2faa72d3f32633056aeda
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-04 19:19:36 -0300

    feat: Imported localStorage manager functions, hooks and custom hooks, as well as the specialized input fields that were just created. Page logic built.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,22 +1,65 @@
-import React from 'react';
-import InputField from '../components/InputField';
-import Button from '../components/Button';
-
-const submitLogin = () => {
-
-};
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useKeyPress } from '../hooks/KeyboardEventListeners';
+import EmailInputField from '../components/inputs/EmailInputField';
+import PasswordInputField from '../components/inputs/PasswordInputField';
+// import InputElement from '../components/inputs/base/InputElement';
+import Button from '../components/inputs/Button';
+import {
+  saveMealsToken, saveCocktailsToken, saveUser,
+} from '../services/LocalStorageManager';
 
 function LoginPage() {
+  const [input, setInput] = useState({ email: '', password: '' });
+  const navigate = useNavigate();
+
+  const isLoginValid = () => {
+    // Regex taken from https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
+    const EMAIL_VALIDATION_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const PW_MINIMUM_LENGTH = 6;
+    const { email, password } = input;
+    return EMAIL_VALIDATION_REGEX.test(email)
+      && password.length >= PW_MINIMUM_LENGTH;
+  };
+
+  const onInputChange = ({ target }) => {
+    setInput({
+      ...input,
+      [target.name]: target.value,
+    });
+  };
+
+  const submitLogin = () => {
+    // Fetch user credentials. If valid, proceed.
+    // Fetch user tokens.
+    saveUser(input.email);
+    saveCocktailsToken(1);
+    saveMealsToken(1);
+    navigate('/foods');
+  };
+
+  useKeyPress('Enter', () => isLoginValid() && submitLogin());
+  useKeyPress('NumpadEnter', () => isLoginValid() && submitLogin());
+
   return (
     <div className="page-wrapper">
-      <section className="page-content  login-page-content">
+      <section className="page-content login-page-content">
         <h1>Login</h1>
-        <InputField type="email" placeholder="Email" />
-        <InputField type="password" placeholder="Password" />
-        <Button type="button" text="Enter" onClick={submitLogin} />
+        <EmailInputField
+          onChange={onInputChange}
+        />
+        <PasswordInputField
+          onChange={onInputChange}
+        />
+        <Button
+          type="button"
+          text="Enter"
+          disabled={!isLoginValid()}
+          onClick={submitLogin}
+          onEnterUp={submitLogin}
+        />
       </section>
     </div>
-
   );
 }
 
