9a3c4157c2a89dbae04ffccbb54a3e32ca1b7eaa
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-05 10:01:00 -0300

    Changed search bar to LabeledInput component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,30 +2,34 @@ import React from 'react';
 import { MagnifyingGlass } from 'phosphor-react';
 import PropTypes from 'prop-types';
 import { defaultIconConfig as ic } from '../helpers';
+import LabeledInput from './inputs/LabeledInput';
 
 function Header({ title }) {
   const onLabelButtonClick = () => {
-
   };
+
+  const searchButton = (
+    <button
+      className="icon-button"
+      type="button"
+      onClick={onLabelButtonClick}
+    >
+      <MagnifyingGlass weight={ic.weight} color={ic.color} size={ic.size} />
+    </button>
+  );
+
   return (
     <section className="header">
       <h1 className="header-title">{title}</h1>
-
-      <div className="input header-search-bar">
-        <button
-          className="icon-button"
-          type="button"
-          onClick={onLabelButtonClick}
-        >
-          <MagnifyingGlass weight={ic.weight} color={ic.color} size={ic.size} />
-        </button>
-        <input
-          className="search-input"
-          type="search"
-          placeholder="Search Recipe"
-        />
-      </div>
-
+      <LabeledInput
+        divClassName="input header-search-bar"
+        className="search-input"
+        type="search"
+        name="search"
+        placeholder="Search Recipe"
+        label={searchButton}
+        labelToRight
+      />
     </section>
   );
 }
