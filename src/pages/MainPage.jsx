3c05859a1ec76efd03d2699b66558907d4bcb4ad
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-04 22:59:28 -0300

    feat: Added Header and Footer to MainPage.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function MainPage() {
   return (
-    <div>MainPage</div>
+    <div className="page-wrapper">
+      <Header />
+      <div className="page-content main-page-content">
+        Main Page
+      </div>
+      <Footer />
+    </div>
   );
 }
 
