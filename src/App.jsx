2b23de2fffb696fbbf3d8e10ef78b37ead726abf
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-05 13:50:27 -0300

    Added the ListsProvider to the App hierarchy.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecipesProvider from './context/RecipesProvider';
+import ListsProvider from './context/ListsProvider';
 import LoginPage from './pages/LoginPage';
 import UserProfile from './pages/UserProfile';
 import MainPage from './pages/MainPage';
@@ -14,38 +15,40 @@ import './App.css';
 
 function App() {
   return (
-    <RecipesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<LoginPage />} />
-          <Route exact path="/foods" element={<MainPage />} />
-          <Route exact path="/drinks" element={<MainPage />} />
-          <Route path="/foods/:foodId" element={<RecipePage />} />
-          <Route path="/drinks/:drinkId" element={<RecipePage />} />
-          <Route path="/foods/:foodId/in-progress" element={<RecipePage />} />
-          <Route path="/drinks/:drinkId/in-progress" element={<RecipePage />} />
-          <Route exact path="/explore" element={<ExplorePage />} />
-          <Route path="/explore/foods" element={<ExplorePage />} />
-          <Route path="/explore/drinks" element={<ExplorePage />} />
-          <Route
-            path="/explore/foods/ingredients"
-            element={<ExploreByIngredients />}
-          />
-          <Route
-            path="/explore/drinks/ingredients"
-            element={<ExploreByIngredients />}
-          />
-          <Route
-            path="/explore/foods/nationalities"
-            component={<ExploreByNationalities />}
-          />
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/done-recipes" element={<UserRecipes />} />
-          <Route path="/favorite-recipes" element={<UserRecipes />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </RecipesProvider>
+    <ListsProvider>
+      <RecipesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<LoginPage />} />
+            <Route exact path="/foods" element={<MainPage />} />
+            <Route exact path="/drinks" element={<MainPage />} />
+            <Route path="/foods/:foodId" element={<RecipePage />} />
+            <Route path="/drinks/:drinkId" element={<RecipePage />} />
+            <Route path="/foods/:foodId/in-progress" element={<RecipePage />} />
+            <Route path="/drinks/:drinkId/in-progress" element={<RecipePage />} />
+            <Route exact path="/explore" element={<ExplorePage />} />
+            <Route path="/explore/foods" element={<ExplorePage />} />
+            <Route path="/explore/drinks" element={<ExplorePage />} />
+            <Route
+              path="/explore/foods/ingredients"
+              element={<ExploreByIngredients />}
+            />
+            <Route
+              path="/explore/drinks/ingredients"
+              element={<ExploreByIngredients />}
+            />
+            <Route
+              path="/explore/foods/nationalities"
+              component={<ExploreByNationalities />}
+            />
+            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/done-recipes" element={<UserRecipes />} />
+            <Route path="/favorite-recipes" element={<UserRecipes />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </RecipesProvider>
+    </ListsProvider>
   );
 }
 
