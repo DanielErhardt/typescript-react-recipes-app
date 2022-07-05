118aa35fe6d090ab5d965136752fa3b14bdea17e
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-04 22:58:44 -0300

    Replaced LabeledInput with a common input tag and a button.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import { MagnifyingGlass } from 'phosphor-react';
 import PropTypes from 'prop-types';
-import LabeledInput from './inputs/LabeledInput';
 import { defaultIconConfig as ic } from '../helpers';
 
 function Header({ title }) {
   const onLabelButtonClick = () => {
 
   };
-
-  const labelButton = (
-    <button
-      type="button"
-      onClick={onLabelButtonClick}
-    >
-      <MagnifyingGlass weight={ic.weight} color={ic.color} size={ic.size} />
-    </button>
-  );
-
   return (
     <section className="header">
       <h1 className="header-title">{title}</h1>
-      <LabeledInput
-        divClassName="header-search-bar"
-        label={labelButton}
-        type="search"
-      />
+
+      <div className="input">
+        <button
+          className="icon-button"
+          type="button"
+          onClick={onLabelButtonClick}
+        >
+          <MagnifyingGlass weight={ic.weight} color={ic.color} size={ic.size} />
+        </button>
+        <input
+          className="search-input"
+          type="search"
+          placeholder="Search Recipe"
+        />
+      </div>
+
     </section>
   );
 }
