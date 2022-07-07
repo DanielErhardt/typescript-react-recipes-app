2b66bd8a363a44a6b2f984ded2b786b4988ebeee
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-06 22:01:37 -0300

    feat: Added recipes categories with the DropDownMenu component.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 import RecipesContext from '../context/RecipesContext';
+import ListsContext from '../context/ListsContext';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RecipesList from '../components/RecipesList';
+import DropDownMenu from '../components/DropDownMenu';
 import { MEALS_TYPE, COCKTAILS_TYPE, fetchAllRecipes } from '../services/RecipesAPI';
 
 function MainPage() {
   const { recipes, setRecipes } = useContext(RecipesContext);
+  const { mealsCategories, cocktailsCategories } = useContext(ListsContext);
   const location = useLocation();
   const currentType = location.pathname.includes('meal') ? MEALS_TYPE : COCKTAILS_TYPE;
+  const categories = currentType === MEALS_TYPE ? mealsCategories : cocktailsCategories;
 
   useEffect(() => {
     const fetch = async () => {
@@ -30,6 +34,7 @@ function MainPage() {
     <div className="page-wrapper">
       <Header title="Recipes" />
       <div className="page-content main-page-content">
+        <DropDownMenu options={categories} label="Category: " />
         <RecipesList />
       </div>
       <Footer />
