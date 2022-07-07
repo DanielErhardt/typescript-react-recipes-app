94177ca1c5d76840768d3761a11f07c04672ec92
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-06 21:58:25 -0300

    feat: Added hooks and a recipes fetch to the MainPage. Imported and placed RecipesList component as well.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
+import RecipesContext from '../context/RecipesContext';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import RecipesList from '../components/RecipesList';
+import { MEALS_TYPE, COCKTAILS_TYPE, fetchAllRecipes } from '../services/RecipesAPI';
 
 function MainPage() {
+  const { recipes, setRecipes } = useContext(RecipesContext);
+  const location = useLocation();
+  const currentType = location.pathname.includes('meal') ? MEALS_TYPE : COCKTAILS_TYPE;
+
+  useEffect(() => {
+    const fetch = async () => {
+      if (recipes.length === 0) {
+        const data = await fetchAllRecipes(currentType);
+        setRecipes(data[currentType]);
+      }
+    };
+
+    fetch();
+
+    return () => {
+      setRecipes([]);
+    };
+  }, []);
+
   return (
     <div className="page-wrapper">
       <Header title="Recipes" />
       <div className="page-content main-page-content">
-        Main Page
+        <RecipesList />
       </div>
       <Footer />
     </div>
