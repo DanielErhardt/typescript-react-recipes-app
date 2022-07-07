c3a9014a210f8c139c018081bc261261f24d43c5
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-07-06 21:55:28 -0300

    feat: Created RecipeCard component core.

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function RecipeCard() {
+function RecipeCard({ recipe }) {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const id = recipe.idMeal || recipe.idDrink;
+  const name = recipe.strMeal || recipe.strDrink;
+  const thumb = recipe.strMealThumb || recipe.strDrinkThumb;
+  const category = recipe.strCategory;
+
   return (
-    <div>RecipeCard</div>
+    <button
+      type="button"
+      className="recipe-card"
+      onClick={() => navigate(`${pathname}/${id}`)}
+    >
+      {thumb}
+      {category}
+      {name}
+    </button>
   );
 }
+
+RecipeCard.defaultProps = {
+  recipe: PropTypes.shape({
+    idMeal: '',
+    idDrink: '',
+    strMeal: '',
+    strDrink: '',
+    strMealThumb: '',
+    strDrinkThumb: '',
+    strCategory: '',
+  }),
+};
+
+RecipeCard.propTypes = {
+  recipe: PropTypes.shape({
+    idMeal: PropTypes.string,
+    idDrink: PropTypes.string,
+    strMeal: PropTypes.string,
+    strDrink: PropTypes.string,
+    strMealThumb: PropTypes.string,
+    strDrinkThumb: PropTypes.string,
+    strCategory: PropTypes.string,
+  }),
+};
 
 export default RecipeCard;
