bb005714c7e82e25fc4cc8138e89b3b1b65bc6d8
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-07-12 13:44:25 -0300

    styles: Changed RecipeList component className.

--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -5,7 +5,7 @@ import RecipesContext from '../context/RecipesContext';
 function RecipesList() {
   const { recipes } = useContext(RecipesContext);
   return (
-    <div className="recipes-card-list">
+    <div className="recipes-list">
       {recipes.map((recipe) => (
         <RecipeCard
           key={recipe.idMeal || recipe.idDrink}
