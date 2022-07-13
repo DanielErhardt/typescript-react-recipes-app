de723ff588e3f8b16a3fb071f58da1175e0fe103
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-07-12 22:34:10 -0300

    feat: RecipesProvider now manages all fetches and provides all possible recipes lists.

--- src/context/RecipesProvider.jsx
+++ src/context/RecipesProvider.jsx
@@ -1,17 +1,72 @@
-import React, { useState, useMemo } from 'react';
+import React, {
+  useState, useMemo, useEffect, useContext,
+} from 'react';
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import RecipesContext from './RecipesContext';
+import ListsContext from './ListsContext';
+import {
+  MEALS_TYPE, COCKTAILS_TYPE,
+  fetchAllRecipes, fetchRecipeById, fetchRecipesByName,
+  fetchRecipesByFirstLetter, fetchRecipesByIngredient,
+  fetchRecipesByCategory, fetchRandomRecipe,
+  fetchMealsByArea,
+} from '../services/RecipesAPI';
 
 function RecipesProvider({ children }) {
+  const { categories, ingredients, nationalites } = useContext(ListsContext);
   const [recipes, setRecipes] = useState([]);
+  const { pathname } = useLocation();
+  const recipeType = pathname.includes('meals') ? MEALS_TYPE : COCKTAILS_TYPE;
 
-  const contextType = useMemo(() => ({
+  const extract = (data) => (data ? data[recipeType] : []);
+
+  const fetchAll = async () => setRecipes(extract(await fetchAllRecipes(recipeType)));
+  const fetchById = async (id) => setRecipes(extract(await fetchRecipeById(recipeType, id)));
+  const fetchByName = async (name) => setRecipes(
+    extract(await fetchRecipesByName(recipeType, name)),
+  );
+  const fetchByFirstLetter = async (letter) => setRecipes(
+    extract(await fetchRecipesByFirstLetter(recipeType, letter)),
+  );
+  const fetchByIngredient = async (ingredient) => setRecipes(
+    extract(ingredients[0] === ingredient
+      ? await fetchAll()
+      : await fetchRecipesByIngredient(recipeType, ingredient)),
+  );
+  const fetchByCategory = async (category) => setRecipes(
+    extract(categories[0] === category
+      ? await fetchAll()
+      : await fetchRecipesByCategory(recipeType, category)),
+  );
+  const fetchMealsByNationality = async (nationality) => setRecipes(
+    extract(nationalites[0] === nationality
+      ? await fetchAll()
+      : await fetchMealsByArea(nationality)),
+  );
+  const fetchRandom = async () => setRecipes(
+    extract(await fetchRandomRecipe(recipeType)),
+  );
+
+  useEffect(() => {
+    if (recipes.length === 0) fetchAll();
+  }, [recipes]);
+
+  const contextValue = useMemo(() => ({
+    fetchAll,
+    fetchById,
+    fetchByName,
+    fetchByFirstLetter,
+    fetchByIngredient,
+    fetchByCategory,
+    fetchMealsByNationality,
+    fetchRandom,
     recipes,
-    setRecipes,
-  }), [recipes]);
+    recipeType,
+  }), [recipes, pathname]);
 
   return (
-    <RecipesContext.Provider value={contextType}>
+    <RecipesContext.Provider value={contextValue}>
       {children}
     </RecipesContext.Provider>
   );
