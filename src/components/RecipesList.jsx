8d045240eaa5dc3d87c919df772da5ece6ec4965
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-07-13 12:49:36 -0300

    feat: RecipesList now redirects to the recipe details page if search return only one recipe and shows a warning if it returns none.

--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -1,12 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import RecipeCard from './RecipeCard';
 import RecipesContext from '../context/RecipesContext';
+import { openModalWindow } from '../helpers';
 
 function RecipesList() {
-  const { recipes } = useContext(RecipesContext);
+  const { recipes, resetRecipes, fetchAll } = useContext(RecipesContext);
+  const navigate = useNavigate();
+  const { pathname: path } = useLocation();
+
+  useEffect(() => {
+    if (recipes) {
+      if (recipes.length === 0) fetchAll();
+      if (recipes.length === 1) {
+        navigate(`${path}/${recipes[0].idMeal || recipes[0].idDrink}`);
+        resetRecipes();
+      }
+    } else {
+      openModalWindow('Could not find any matching recipes.');
+      fetchAll();
+    }
+  }, [recipes]);
+
   return (
     <div className="recipes-list">
-      {recipes.map((recipe) => (
+      {recipes && recipes.map((recipe) => (
         <RecipeCard
           key={recipe.idMeal || recipe.idDrink}
           recipe={recipe}
