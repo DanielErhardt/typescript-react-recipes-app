f1be06882c3ab7cb3bf431a10cff64c440a3b14c
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-07-12 22:35:24 -0300

    Removed all fetch responsibilities from MainPage.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,51 +1,17 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useContext } from 'react';
 import RecipesContext from '../context/RecipesContext';
 import ListsContext from '../context/ListsContext';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RecipesList from '../components/RecipesList';
 import DropDownMenu from '../components/DropDownMenu';
-import {
-  MEALS_TYPE, COCKTAILS_TYPE, fetchAllRecipes, fetchRecipesByCategory,
-} from '../services/RecipesAPI';
 
 function MainPage() {
-  const [selectedCategory, setSelectedCategory] = useState('All');
-  const { recipes, setRecipes } = useContext(RecipesContext);
-  const { mealsCategories, cocktailsCategories } = useContext(ListsContext);
-  const { pathname } = useLocation();
-  const currentType = pathname.includes('meal') ? MEALS_TYPE : COCKTAILS_TYPE;
-  const categories = currentType === MEALS_TYPE ? mealsCategories : cocktailsCategories;
-
-  useEffect(() => {
-    const fetch = async () => {
-      switch (selectedCategory) {
-        case 'All':
-          if (recipes.length === 0) {
-            const data = await fetchAllRecipes(currentType);
-            setRecipes(data[currentType]);
-          }
-          break;
-
-        default: {
-          const data = await fetchRecipesByCategory(currentType, selectedCategory);
-          setRecipes(data[currentType]);
-          break;
-        }
-      }
-    };
-
-    fetch();
-
-    return () => {
-      setRecipes([]);
-    };
-  }, [selectedCategory]);
+  const { fetchByCategory } = useContext(RecipesContext);
+  const { categories } = useContext(ListsContext);
 
   const onCategoryChanged = ({ target: { value } }) => {
-    setRecipes([]);
-    setSelectedCategory(value);
+    fetchByCategory(value);
   };
 
   return (
