68d9a3868cc624352221a6f7f47a670b6b541bdc
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-07-15 22:41:04 -0300

    Added stateful arrays and useEffect to get ingredients and measures as well as to keep track of the recipe progress.

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { loadRecipeProgress, saveRecipeProgress } from '../services/LocalStorageManager';
+
 const extractList = (recipe, keyName) => Object.entries(recipe)
   .filter((e) => e[0].includes(keyName))
   .map((m) => m[1])
   .filter((m) => m && m !== ' ') || [];
 
 function IngredientsList({ recipe, onProgressChanged }) {
+  const [ingredients, setIngredients] = useState([]);
+  const [measures, setMeasures] = useState([]);
+  const [checkedIngredients, setCheckedIngredients] = useState([]);
   const { pathname: path } = useLocation();
-  const isInProgress = path.includes('progress');
+  const isInProgressPage = path.includes('progress');
+
+  useEffect(() => {
+    setIngredients(extractList(recipe, 'Ingredient'));
+    setMeasures(extractList(recipe, 'Measure'));
+    setCheckedIngredients(
+      loadRecipeProgress(recipe),
+    );
+  }, [recipe]);
+
 
   return (
     <ul style={{
