093104a9f6306f51e105ff1ff17406817846f4a4
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-07-15 22:37:56 -0300

    feat: Created function to extract arrays of Ingredients and Measures from recipe object.

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
+const extractList = (recipe, keyName) => Object.entries(recipe)
+  .filter((e) => e[0].includes(keyName))
+  .map((m) => m[1])
+  .filter((m) => m && m !== ' ') || [];
 
 function IngredientsList({ recipe, onProgressChanged }) {
   const { pathname: path } = useLocation();
