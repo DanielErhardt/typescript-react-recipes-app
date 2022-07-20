5d3bf55963036005f133980aaad5ffd91c6abea2
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-07-20 12:39:22 -0300

    refactor: Replaced specific fetching functions with filtering functions in RecipesProvider.

--- src/context/RecipesProvider.jsx
+++ src/context/RecipesProvider.jsx
@@ -5,13 +5,15 @@ import RecipesContext from './RecipesContext';
 import ListsContext from './ListsContext';
 import {
   MEALS_TYPE, COCKTAILS_TYPE,
-  fetchAllRecipes, fetchRecipesByName,
-  fetchRecipesByFirstLetter, fetchRecipesByIngredient,
+  fetchAllRecipes,
+  // fetchRecipesByName,
+  // fetchRecipesByFirstLetter, fetchRecipesByIngredient,
   fetchRecipesByCategory, fetchMealsByArea,
 } from '../services/RecipesAPI';
 
 function RecipesProvider({ children }) {
-  const { categories, ingredients, nationalites } = useContext(ListsContext);
+  // const { categories, ingredients, nationalites } = useContext(ListsContext);
+  const { categories, nationalites } = useContext(ListsContext);
   const [recipes, setRecipes] = useState([]);
   const { pathname } = useLocation();
   const recipeType = pathname.includes('meals') ? MEALS_TYPE : COCKTAILS_TYPE;
@@ -26,17 +28,17 @@ function RecipesProvider({ children }) {
     if (invert) setRecipes(extract(await fetchAllRecipes(invertedType), invertedType));
     else setRecipes(extract(await fetchAllRecipes(recipeType)));
   };
-  const fetchByName = async (name) => setRecipes(
-    extract(await fetchRecipesByName(recipeType, name)),
-  );
-  const fetchByFirstLetter = async (letter) => setRecipes(
-    extract(await fetchRecipesByFirstLetter(recipeType, letter)),
-  );
-  const fetchByIngredient = async (ingredient) => setRecipes(
-    extract(ingredients[0] === ingredient
-      ? await fetchAll()
-      : await fetchRecipesByIngredient(recipeType, ingredient)),
-  );
+  // const fetchByName = async (name) => setRecipes(
+  //   extract(await fetchRecipesByName(recipeType, name)),
+  // );
+  // const fetchByFirstLetter = async (letter) => setRecipes(
+  //   extract(await fetchRecipesByFirstLetter(recipeType, letter)),
+  // );
+  // const fetchByIngredient = async (ingredient) => setRecipes(
+  //   extract(ingredients[0] === ingredient
+  //     ? await fetchAll()
+  //     : await fetchRecipesByIngredient(recipeType, ingredient)),
+  // );
   const fetchByCategory = async (category) => setRecipes(
     extract(categories[0] === category
       ? await fetchAll()
@@ -48,16 +50,41 @@ function RecipesProvider({ children }) {
       : await fetchMealsByArea(nationality)),
   );
 
+  const filterByName = async (name) => {
+    setRecipes(recipes.filter((r) => {
+      const recipeName = r.strMeal || r.strDrink;
+      return recipeName.toLowerCase().includes(name.toLowerCase());
+    }));
+  };
+
+  const filterByIngredient = async (ingredient) => {
+    setRecipes(recipes.filter((r) => {
+      const values = Object.values(r);
+      return values.some((v) => typeof v === 'string' && v.toLowerCase().includes(ingredient.toLowerCase()));
+    }));
+  };
+
+  const filterByFirstLetter = async (letter) => {
+    setRecipes(recipes.filter((r) => {
+      const firstLetter = (r.strMeal || r.strDrink).charAt(0);
+      return firstLetter.toLowerCase() === letter.toLowerCase();
+    }));
+  };
+
   const contextValue = useMemo(() => ({
     fetchAll,
-    fetchByName,
-    fetchByFirstLetter,
-    fetchByIngredient,
+    // fetchByName,
+    // fetchByFirstLetter,
+    // fetchByIngredient,
     fetchByCategory,
     fetchMealsByNationality,
+    filterByName,
+    filterByIngredient,
+    filterByFirstLetter,
     recipeType,
     recipes,
     resetRecipes: () => setRecipes([]),
+    isInMealsPage: recipeType === MEALS_TYPE,
   }), [recipes, pathname]);
 
   return (
