b3c1bd559169661331d1d377302ed4521c79dba2
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-07-20 12:37:29 -0300

    refactor: Removed DropDownMenu component from MainPage.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,24 +1,18 @@
 import React, { useContext } from 'react';
 import RecipesContext from '../context/RecipesContext';
-import ListsContext from '../context/ListsContext';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RecipesList from '../components/RecipesList';
-import DropDownMenu from '../components/DropDownMenu';
 
 function MainPage() {
-  const { fetchByCategory } = useContext(RecipesContext);
-  const { categories } = useContext(ListsContext);
+  const { isInMealsPage } = useContext(RecipesContext);
 
-  const onCategoryChanged = ({ target: { value } }) => {
-    fetchByCategory(value);
-  };
+  const getTitle = () => (isInMealsPage ? 'Meals' : 'Cocktails');
 
   return (
     <>
-      <Header title="Recipes" />
+      <Header title={getTitle()} showSearchBar />
       <main>
-        <DropDownMenu options={categories} label="Category: " onChange={onCategoryChanged} />
         <RecipesList />
       </main>
       <Footer />
