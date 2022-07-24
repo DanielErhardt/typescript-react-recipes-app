76affd486fbd573cf28f0dee0e30551f68b5940c
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-07-24 17:42:32 -0300

    RecipesList: Removed ModalWindow from component.

--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -1,14 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import RecipeCard from './RecipeCard';
 import RecipesContext from '../context/RecipesContext';
-import ModalWindow from './ModalWindow';
 
 function RecipesList() {
   const { recipes, resetRecipes, fetchAll } = useContext(RecipesContext);
   const navigate = useNavigate();
   const { pathname: path } = useLocation();
-  const [modalMessage, setModalMessage] = useState('');
 
   useEffect(() => {
     if (recipes) {
@@ -18,14 +16,12 @@ function RecipesList() {
         resetRecipes();
       }
     } else {
-      setModalMessage('Could not find any matching recipes.');
       fetchAll();
     }
   }, [recipes]);
 
   return (
     <div className="recipes-list">
-      <ModalWindow message={modalMessage} onClose={() => setModalMessage('')} />
       {recipes && recipes.map((recipe) => (
         <RecipeCard
           key={recipe.idMeal || recipe.idDrink}
