aa3ec7f237eb90bd0a70d9b903afa0d7b64dae0d
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-07-28 16:26:10 -0300

    feat: Created a @types folder and an index file with some types.

---
+++ src/@types/index.ts
@@ -0,0 +1,20 @@
+import Recipe from '../classes/Recipe';
+
+export type APIObjectType = {
+  [key: string]: string;
+};
+
+export type APIResponseType = {
+  [key: string]: APIObjectType[],
+};
+
+export type RecipesContextType ={
+  recipes: Recipe[];
+  updateRecipes: (apiResponse: APIResponseType) => void; // eslint-disable-line no-unused-vars
+  resetRecipes: () => void;
+  getRecipeType: () => string;
+  isSearchingForMeals: () => boolean;
+  categories: string[];
+  ingredients: string[];
+  nationalities: string[] | null;
+};
