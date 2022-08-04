c398227467fdd60d4d43a0a0ad02bd785a319ec7
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-04 15:06:28 -0300

    refactor/RecipePage: Finished transition to Typescript.

---
+++ src/pages/RecipePage.tsx
@@ -0,0 +1,99 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import RecipesContext from '../context/RecipesContext';
+import { fetchRecipeById, fetchAllRecipes } from '../services/RecipesAPI';
+import IngredientsList from '../components/IngredientsList';
+import RecipeCard from '../components/RecipeCard';
+import Recipe from '../classes/Recipe';
+
+function RecipePage(): JSX.Element {
+  const [finished, setFinished] = useState<boolean>(false);
+  const [recipe, setRecipe] = useState<Recipe>(new Recipe());
+  const {
+    recipes, updateRecipes, getRecipeType, resetRecipes,
+  } = useContext(RecipesContext);
+  const { id } = useParams();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const getRecommended = (): Recipe[] => (recipes ? recipes.slice(0, 6) : []);
+  const isInProgressPage: boolean = pathname.includes('progress');
+
+  useEffect(() => {
+    const fetch = async () => {
+      const data = await fetchRecipeById(getRecipeType(), id || '');
+
+      // TODO fix: this getRecipeType is also being inverted, and it shouldn't
+      // Create getInvertedRecipeType on RecipesProvider
+
+      setRecipe(new Recipe(data[getRecipeType()][0]));
+      updateRecipes(await fetchAllRecipes(getRecipeType(true)));
+    };
+    fetch();
+
+    return () => {
+      resetRecipes();
+    };
+  }, []);
+
+  const finishRecipe = (): void => {
+    navigate('/done-recipes');
+  };
+
+  return (
+    <main className="details-page">
+      <section className="details-page-section details-header">
+        <div className="section-background">
+          <img className="details-image" src={recipe.thumb} alt="Recipe Thumb" />
+          <div className="details-name-bar">
+            <h1>{recipe.name}</h1>
+            {recipe.alcoholic ? <p>{`${recipe.category} (${recipe.alcoholic})`}</p> : <p>{recipe.category}</p>}
+          </div>
+        </div>
+      </section>
+      <section className="details-page-section">
+        <h3>Ingredients</h3>
+        <div className="section-background padded">
+          <IngredientsList recipe={recipe} setFinished={setFinished} />
+        </div>
+      </section>
+      <section className="details-page-section">
+        <h3>Instructions</h3>
+        <div className="section-background padded">
+          <p>{recipe.instructions}</p>
+        </div>
+      </section>
+      {!isInProgressPage && (
+      <>
+        <section className="details-page-section">
+          <h3>Video</h3>
+          <ReactPlayer width="100%" height="100%" url={recipe.youtubeLink} />
+        </section>
+        <section className="details-page-section">
+          <h3>Recommended</h3>
+          <div className="horizontal-scroll">
+            {getRecommended().map((r) => (
+              <RecipeCard
+                key={r.id}
+                recipe={r}
+              />
+            ))}
+          </div>
+        </section>
+      </>
+      )}
+      {(isInProgressPage || (!isInProgressPage && !recipe.isDone())) && (
+      <button
+        type="button"
+        onClick={isInProgressPage ? () => finishRecipe() : () => navigate(`${pathname}/in-progress`)}
+        disabled={isInProgressPage ? !finished : false}
+      >
+        { !isInProgressPage && (recipe.isInProgress() ? 'Continue Recipe' : 'Start Recipe') }
+        { isInProgressPage && 'Finish Recipe'}
+      </button>
+      )}
+    </main>
+  );
+}
+
+export default RecipePage;
