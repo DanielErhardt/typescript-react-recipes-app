64c05edda62cfb2a498aae45a402ae21357ecc60
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-04 15:32:38 -0300

    refactor/MainPage: Replaced isSearchingForMeals() with getRecipeType().

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,11 +3,12 @@ import RecipesContext from '../context/RecipesContext';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RecipesList from '../components/RecipesList';
+import { MEALS_TYPE } from '../services/RecipesAPI';
 
 function MainPage(): JSX.Element {
-  const { isSearchingForMeals } = useContext(RecipesContext);
+  const { getRecipeType } = useContext(RecipesContext);
 
-  const getTitle = (): string => (isSearchingForMeals() ? 'Meals' : 'Cocktails');
+  const getTitle = (): string => (getRecipeType() === MEALS_TYPE ? 'Foods' : 'Drinks');
 
   return (
     <>
