6d59fcd0c0e5deaaec806c46253a420ce4bba136
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-04 15:11:42 -0300

    feat: Finished implementation of Recipe class.

--- src/classes/Recipe.ts
+++ src/classes/Recipe.ts
@@ -1,4 +1,7 @@
 import { APIObjectType } from '../@types';
+import { loadRecipe, saveRecipe } from '../services/LocalStorageManager';
+import { FOODS_TYPE, DRINKS_TYPE } from '../services/RecipesAPI';
+import type { Ingrendient } from '../@types';
 
 // eslint-disable-next-line max-len
 // Regex taken from https://stackoverflow.com/questions/650022/how-do-i-split-a-string-with-multiple-separators-in-javascript
@@ -12,27 +15,72 @@ const extractList = (recipe: APIObjectType, keyName:string): string[] => (
 );
 
 export default class Recipe {
-  id: string;
-  name: string;
-  thumb: string;
-  category: string;
-  description: string;
-  alcoholic: string | undefined;
-  youtubeLink: string;
-  ingredients: string[];
-  measures: string[];
-  tags: string[];
+  readonly id: string;
+  readonly recipeType: string;
+  readonly name: string;
+  readonly thumb: string;
+  readonly category: string;
+  readonly instructions: string;
+  readonly alcoholic: string | undefined;
+  readonly youtubeLink: string;
+  ingredients: readonly Ingrendient[];
+  tags: readonly string[];
+  private checkedIngredients: string[] = [];
+  private favorite: boolean = false;
 
-  constructor(recipe: APIObjectType) {
-    this.id = recipe.idMeal || recipe.idDrink;
-    this.name = recipe.strMeal || recipe.strDrink;
-    this.thumb = recipe.strMealThumb || recipe.strDrinkThumb;
-    this.category = recipe.strCategory;
-    this.description = recipe.strDescription;
-    this.alcoholic = recipe.strAlcoholic;
-    this.youtubeLink = recipe.strYoutube;
-    this.ingredients = extractList(recipe, 'Ingredients');
-    this.measures = extractList(recipe, 'Measures');
-    this.tags = recipe.strTags ? recipe.strTags.split(tagsSeparator) : [];
+  constructor();
+  constructor(recipe: APIObjectType);
+  constructor(recipe?: APIObjectType) {
+    this.id = recipe?.idMeal || recipe?.idDrink || '';
+    this.name = recipe?.strMeal || recipe?.strDrink || '';
+    this.thumb = recipe?.strMealThumb || recipe?.strDrinkThumb || '';
+    this.category = recipe?.strCategory || '';
+    this.instructions = recipe?.strInstructions || '';
+    this.alcoholic = recipe?.strAlcoholic;
+    this.youtubeLink = recipe?.strYoutube || '';
+    this.tags = recipe?.strTags ? recipe.strTags.split(tagsSeparator) : [];
+
+    const getType = (r: APIObjectType): string => (r.idMeal ? FOODS_TYPE : DRINKS_TYPE);
+    this.recipeType = recipe ? getType(recipe) : '';
+
+    const ingNames = extractList(recipe || {}, 'Ingredient');
+    const ingMeasures = extractList(recipe || {}, 'Measure');
+    const ings: Ingrendient[] = [];
+    ingNames.forEach((n, index) => ings.push({ name: n, measure: ingMeasures[index] }));
+
+    this.ingredients = ings;
+
+    const savedRecipe = loadRecipe(this.id);
+    if (savedRecipe) {
+      this.checkedIngredients = savedRecipe.checkedIngredients;
+      this.favorite = savedRecipe.favorite;
+    }
   }
+
+  isDone = (): boolean => this.ingredients.every((ing) => this.checkedIngredients.includes(ing.name));
+
+  save = (): void => saveRecipe(this);
+
+  isFavorite = (): boolean => this.favorite;
+
+  setFavorite = (favorite: boolean): void => {
+    this.favorite = favorite;
+    this.save();
+  };
+
+  checkIngredient = (ingredient: string, isChecked: boolean): void => {
+    if (!this.ingredients.some((ing) => ing.name === ingredient)) {
+      throw Error('Trying to check/uncheck ingredient that doesn\'t belong to the recipe.');
+    }
+
+    this.checkedIngredients = this.checkedIngredients.filter((ing) => ing !== ingredient); // for making sure it's not added twice
+    if (isChecked) this.checkedIngredients.push(ingredient);
+    this.save();
+  };
+
+  // isIngredientChecked = (ingredient: string): boolean => this.checkedIngredients.includes(ingredient);
+
+  getCheckedIngredients = (): string[] => this.checkedIngredients;
+
+  isInProgress = (): boolean => this.checkedIngredients.length > 0;
 }
