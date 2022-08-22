64d086cb900564e3d78f71194b38b1f0d27d4d1d
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-22 11:08:26 -0300

    style: Created a styled component for RecipesList.

---
+++ src/styles/components/RecipesList.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const RecipesList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 6px;
+  gap: 6px;
+  flex-wrap: wrap;
+`;
+
+export default RecipesList;
