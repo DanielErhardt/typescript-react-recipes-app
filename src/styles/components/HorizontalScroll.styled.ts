effb858bac20420e813939ef38602ff52eebf2c0
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-22 11:06:15 -0300

    style: Created a styled component for HorizontalScroll.

---
+++ src/styles/components/HorizontalScroll.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const HorizontalScroll = styled.div`
+  display: flex;
+  gap: 6px;
+  min-height: 220px;
+  overflow: scroll;
+`;
+
+export default HorizontalScroll;
